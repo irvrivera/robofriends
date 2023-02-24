eb691124eecc7e457937ed563c27871154b760e8
Irving Rivera <irvrivera67@gmail.com>
2023-02-24 12:50:10 -0500

    add Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App () {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: "",
+  //   };
+  // }
+  const[robots, setRobots] = useState([])
+  const[searchfield, setSearchfield]= useState()
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) =>({setRobots: (users )}));
+    },[])
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield({ event,target,value })
   };
 
-  render() {
-	const { robots, searchfield} = this.state;
+  
+	
 	const filteredRobots = robots.filter((robot) => {
       return robot.name
         .toLocaleLowerCase()
@@ -36,7 +44,7 @@ class App extends Component {
     (
         <div className="tc">
           <h1 className="f1">Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundry>
              <CardList robots={filteredRobots} />
@@ -45,7 +53,7 @@ class App extends Component {
         </div>
       );
     }
-  }
+  
 
 
 export default App;
--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,22 +1,22 @@
-import React from "react";
+// import React from "react";
 
-class ErrorBoundry extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
+// class ErrorBoundry extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hasError: false,
+//     };
+//   }
 
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-  }
-  return() {
-    if (this.state.hasError) {
-      return <h1> Oppps!!! That is not good</h1>;
-    }
-    return this.props.children;
-  }
-}
+//   componentDidCatch(error, info) {
+//     this.setState({ hasError: true });
+//   }
+//   return() {
+//     if (this.state.hasError) {
+//       return <h1> Oppps!!! That is not good</h1>;
+//     }
+//     return this.props.children;
+//   }
+// }
 
-export default ErrorBoundry;
+// export default ErrorBoundry;
